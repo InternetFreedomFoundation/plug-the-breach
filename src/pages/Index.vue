c26ece133922bd753cd5fc5a307ef6114226d221
Anoop B <16367697+anoop-b@users.noreply.github.com>
2022-07-06 12:49:54 +0530

    fix: font color

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,7 +2,7 @@
   <Layout>
     <div class="overflow-hidden relative">
       <div class="text-start w-1/2 py-12 px-4 sm:px-6 lg:py-16 lg:px-8 z-20">
-        <h2 class="text-3xl font-extrabold text-black dark:text-white sm:text-4xl">
+        <h2 class="text-3xl font-extrabold sm:text-4xl">
           <span class="block">Tracking</span>
           <span class="block text-teal-800">Data breaches in India</span>
         </h2>
