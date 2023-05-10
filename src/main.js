8431eb2e08f08c4165cbd8f9a6f88dfd9e8d4626
IKA3RUS <w@abhishekrein.xyz>
2023-05-10 13:34:05 +0530

    feat: Improve search with router params and better UX

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 
+import VueFeather from 'vue-feather';
 import DefaultLayout from './layouts/Default.vue';
 
-// Import Tailwind CSS
-require('./main.css');
+require('./main.css'); // Import Tailwind CSS
 
 export default function (Vue, { router, head, isClient }) {
-  // Set default layout as a global component
   Vue.component('Layout', DefaultLayout);
+  Vue.component('VueFeather', VueFeather);
 }
--- src/pages/Breaches.vue
+++ src/pages/Breaches.vue
@@ -1,21 +1,41 @@
 <template>
   <Layout>
     <div class="">
-      <input class="stroke-3
+      <div class="stroke-3
                   ring-offset
-                  mb-8
+                  my-8
+                  flex
                   w-full
+                  justify-between
                   rounded-full
                   border-2
                   border-emerald-900
                   bg-white
-                  p-6
+                  p-1
+                  pl-4
                   outline-none
-                  ring-emerald-300
-                  ring-offset-1
                   placeholder:text-gray-300
-                  focus:ring-2 " v-model="searchQuery" type="text"
-        placeholder="Search a breach">
+                  " @click="$refs.searchInput.focus()">
+        <input class="grow
+                      bg-transparent
+                      focus:outline-none"
+        type="text" :value="searchQuery" @keydown.enter="updateSearchQuery($event.target.value)"
+          placeholder="Search a breach" ref="searchInput">
+        <button class="aspect-square
+                      rounded-full
+                      bg-emerald-900
+                      p-4
+                      text-white
+                      hover:bg-emerald-800
+                      focus:outline-none
+                      focus:ring-2" @click="updateSearchQuery($refs.searchInput.value)">
+          <VueFeather type="search" />
+        </button>
+      </div>
+      <div class="my-8">
+        <h4 v-if="searchQuery && searchResults.length > 0">Showing search results for {{ searchQuery }}</h4>
+        <h4 v-else-if="searchQuery && searchResults.length == 0">Oops! We couldn't find anything for "{{ searchQuery }}". Showing all breaches.</h4>
+      </div>
       <div class="grid grid-cols-4 gap-4">
         <div class="pb-100
                   flex
@@ -65,14 +85,10 @@ export default {
   data: () => ({
     searchQuery: '',
   }),
+  created() {
+    if (this.$route.query.search) { this.searchQuery = this.$route.query.search; }
+  },
   computed: {
-    searchResults() {
-      const query = this.searchQuery;
-      if (this.searchQuery.length < 3) {
-        return [];
-      }
-      return this.$search.search({ query: this.searchQuery, limit: 5 });
-    },
     /**
      * Maps the GraphQL array of objects to a new array of objects with simplified property
      * access by removing the need to include 'node' in the property path.
@@ -81,9 +97,27 @@ export default {
     mappedBreaches() {
       return this.$page.breaches.edges.map((edge) => ({ ...edge.node }));
     },
+    searchResults() {
+      if (this.searchQuery.length < 3) return [];
+      return this.$search.search({ query: this.searchQuery, limit: 5 });
+    },
     breachList() {
       if (this.searchResults.length > 0) return this.searchResults;
       return this.mappedBreaches;
+    },
+  },
+  methods: {
+    updateSearchQuery(query) {
+      this.searchQuery = query;
+      if (this.$route.query.search !== this.searchQuery) this.updateURL();
+    },
+    updateURL() {
+      this.$router.push({
+        path: '/breaches',
+        query: {
+          search: this.searchQuery,
+        },
+      });
     },
   },
 };
