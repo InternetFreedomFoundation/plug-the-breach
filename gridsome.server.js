6f95362bf354126fa2f00f752aed49e4299b1af7
Anoop B <16367697+anoop-b@users.noreply.github.com>
2023-06-14 15:32:42 +0300

    feat: pocketbase

--- gridsome.server.js
+++ gridsome.server.js
@@ -4,6 +4,7 @@
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run `gridsome develop`
+const PocketBase = require('pocketbase/cjs')
 
 module.exports = function (api) {
   api.loadSource(async (store) => {
@@ -16,4 +17,22 @@ module.exports = function (api) {
     //   component: "./src/templates/MyPage.vue",
     // });
   });
+
+  api.loadSource(async actions => {
+    const pb = new PocketBase('https://content.internetfreedom.in');
+    const records = await pb.collection('plug_the_breach').getFullList({
+      sort: '-created',
+  });
+
+    const collection = actions.addCollection({
+      typeName: 'PTB'
+    })
+
+    for (const item of records) {
+      collection.addNode({
+        id: item.id,
+        company: item.company
+      })
+    }
+  })
 };
